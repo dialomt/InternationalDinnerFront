1b4365065b1ce17120b479df17ec62648a1643e8
soymariavillaverde <soymariavillaverde@gmail.com>
2024-03-01 16:09:41 +0100

    beging project with react and tailwind :tada:

---
+++ src/components/trial.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css'; // Asegúrate de importar tu archivo CSS
+import '../index.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      <h1 className="text-2xl font-bold">Hola, mundo!</h1>
+    </div>
+  );
+}
+
+export default App;
